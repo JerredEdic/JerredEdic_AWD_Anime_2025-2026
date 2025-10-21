eeb008418acdea46b165bca91891d461719564f9
Jerred Edic (Student) <N00230861@iadt.ie>
2025-10-21 15:49:43 +0100

    Made the MCRS for Anime, Currently working on routes

---
+++ database/seeders/AnimeSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use App\models\Anime;
+
+class AnimeSeeder extends Seeder{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $currentTimestamp = Carbon::now();
+
+        Anime::insert([
+            [
+                'name' => 'Demon Slayer',
+                'description' => 'A boy travels with their sister to get revenge against the man who killed their family',
+                'episodes' => 63,
+                'image' => 'demonSlayer.jpg',
+                'created_at' => $currentTimestamp,
+                'updated_at' => $currentTimestamp
+            ]
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AnimeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    //Anime Routes
+    Route::get('/animes', [AnimeController::class, 'index'])->name('animes.index');
+    Route::get('/animes/create', [AnimeController::class, 'create'])->name('animes.create');
+    Route::get('/animes/{anime}', [AnimeController::class, 'show'])->name('animes.show');
+    Route::post('/animes', [AnimeController::class, 'store'])->name('animes.store');
+
+    Route::get('/animes/{anime}/edit', [AnimeController::class, 'edit'])->name('animes.edit');
+    Route::put('/animes/{anime}', [AnimeController::class, 'update'])->name('animes.update');
+    Route::delete('/animes/{anime}', [AnimeController::class, 'destroy'])->name('animes.destroy');
+});
+
+require __DIR__.'/auth.php';
